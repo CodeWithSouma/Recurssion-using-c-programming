de190b0e66bd345ac391777e9c22d0957ea9dabb
Souma <souma.hitech@gmail.com>
2021-02-24 23:48:44 +0530

    complete array and dec inc problem

--- recurssion.java
+++ recurssion.java
@@ -1,8 +1,14 @@
+
 public class recurssion {
 
     public static void main(String[] args) {
-        int result = fibonacci(5);
-        System.out.println(result);
+        // int [] array = {10,20,30,40,50};
+        // var result = arrayIsSortedOrNot(array,array.length);
+        // System.out.println(result);
+
+        printDec(5);
+        System.out.println();
+        printInc(5);
         
     }
 
@@ -44,5 +50,27 @@ public class recurssion {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
+    // check a given array is sorted or not using recurssion
+    // [1,2,3,4,5]
+    public static boolean arrayIsSortedOrNot(int [] array, int size){
+        if(size == 0 || size == 1) return true;
+        
+        if(array [size -1] < array[size - 2]) return false;
+
+        return arrayIsSortedOrNot(array, size - 1);
+    }
+
+    // print decreasing order
+    public static void printDec(int n){
+        if(n == 0) return;
+        System.out.print(n + " \t");
+        printDec(n - 1);
+    }
+    // print increasing order
+    public static void printInc(int n){
+        if(n == 0) return;
+        printInc(n - 1);
+        System.out.print(n + " \t");
+    }
 
 }
